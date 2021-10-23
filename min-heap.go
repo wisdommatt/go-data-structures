65d566d38c1872fd415f539d8c7e7c20772bc658
Wisdom Matthew <talk2wisdommatt@gmail.com>
2021-10-24 00:28:48 +0100

    feat(heap): add min-heap data structure implementation

---
+++ min-heap.go
@@ -0,0 +1,185 @@
+package datastructures
+
+import (
+	"errors"
+	"fmt"
+)
+
+// MinHeap represents the min-heap data structure.
+//
+// this heap is a min heap.
+type MinHeap struct {
+	hashTable map[float64][]int
+	items     []float64
+	length    int
+}
+
+// NewMinHeap returns a new min-heap data structure.
+func NewMinHeap() *MinHeap {
+	return &MinHeap{
+		hashTable: make(map[float64][]int),
+	}
+}
+
+// Insert adds a new item to the heap.
+func (h *MinHeap) Insert(item float64) *MinHeap {
+	h.items = append(h.items, item)
+	h.length++
+	if _, ok := h.hashTable[item]; !ok {
+		h.hashTable[item] = []int{}
+	}
+	h.hashTable[item] = append(h.hashTable[item], h.length-1)
+	h.bubbleUpFromIndex(h.length - 1)
+	return h
+}
+
+func (h *MinHeap) bubbleUpFromIndex(index int) {
+	if h.length < 2 {
+		return
+	}
+	parentIndex := h.getNodeParentIndex(index)
+	if h.items[index] < h.items[parentIndex] {
+		h.swap(parentIndex, index, true)
+	}
+}
+
+func (h *MinHeap) getNodeParentIndex(nodeIndex int) int {
+	// indexes 0 - 3 are edge cases.
+	if nodeIndex < 3 {
+		return 0
+	}
+	if nodeIndex == 3 {
+		return 1
+	}
+	parentIndex := ((nodeIndex - 2) / 2)
+	if (nodeIndex % 2) != 0 {
+		parentIndex = ((nodeIndex - 1) / 2)
+	}
+	return parentIndex
+}
+
+// Poll removes the root element from the heap.
+func (h *MinHeap) Poll() error {
+	return h.removeAtPosition(0)
+}
+
+// removeAtPosition removes an element from the specified position.
+func (h *MinHeap) removeAtPosition(position int) error {
+	if h.length == 0 {
+		return errors.New("heap is empty")
+	}
+	h.removePositionFromHashTable(h.items[position], position)
+	if h.length == 1 {
+		h.items = nil
+		h.length--
+		return nil
+	}
+	h.items[position] = h.items[h.length-1]
+	h.items = h.items[:h.length-1]
+	h.length--
+	// checking if the element is the last element in the heap.
+	//
+	// if the element is the last element in the heap, there is no
+	// need to bubble down.
+	if position != h.length {
+		h.removePositionFromHashTable(h.items[position], h.length)
+		h.hashTable[h.items[position]] = append(h.hashTable[h.items[position]], position)
+		h.bubbleDownFromIndex(position)
+	}
+	return nil
+}
+
+func (h *MinHeap) bubbleDownFromIndex(index int) {
+	if h.length < 2 {
+		return
+	}
+	lcIndex, rcIndex := h.getNodeChildrenIndexes(index)
+	if h.items[lcIndex] < h.items[index] && h.items[lcIndex] < h.items[rcIndex] {
+		h.swap(index, lcIndex, false)
+		return
+	}
+	if h.items[rcIndex] < h.items[index] {
+		h.swap(index, rcIndex, false)
+	}
+}
+
+func (h *MinHeap) removePositionFromHashTable(item float64, position int) {
+	arr := h.hashTable[item]
+	newArr := []int{}
+	for _, v := range arr {
+		if v != position {
+			newArr = append(newArr, v)
+		}
+	}
+	h.hashTable[item] = newArr
+}
+
+func (h *MinHeap) getNodeChildrenIndexes(nodeIndex int) (int, int) {
+	leftChildIndex := (2 * nodeIndex) + 1
+	rightChildIndex := (2 * nodeIndex) + 2
+	if (h.length - 1) < leftChildIndex {
+		return nodeIndex, nodeIndex
+	}
+	if (h.length - 1) < rightChildIndex {
+		return leftChildIndex, nodeIndex
+	}
+	return leftChildIndex, rightChildIndex
+}
+
+func (h *MinHeap) swap(index1, index2 int, bubbleUp bool) {
+	parentValue := h.items[index1]
+	childValue := h.items[index2]
+	h.items[index1] = childValue
+	h.items[index2] = parentValue
+	// updating the indexes in the hash table.
+	h.removePositionFromHashTable(parentValue, index1)
+	h.removePositionFromHashTable(childValue, index2)
+	h.hashTable[parentValue] = append(h.hashTable[parentValue], index2)
+	h.hashTable[childValue] = append(h.hashTable[childValue], index1)
+
+	if bubbleUp {
+		newParentIndex := h.getNodeParentIndex(index1)
+		if h.items[index1] < h.items[newParentIndex] {
+			h.swap(newParentIndex, index1, true)
+		}
+	}
+
+	bubbleDown := !bubbleUp
+	if bubbleDown {
+		lcIndex, rcIndex := h.getNodeChildrenIndexes(index2)
+		if h.items[lcIndex] < h.items[index2] && h.items[lcIndex] < h.items[rcIndex] {
+			h.swap(index2, lcIndex, false)
+		}
+		if h.items[rcIndex] < h.items[index2] {
+			h.swap(index2, rcIndex, false)
+		}
+	}
+}
+
+// Remove removes an item from the heap.
+func (h *MinHeap) Remove(item float64) error {
+	if h.length == 0 {
+		return errors.New("heap is empty")
+	}
+	positions, ok := h.hashTable[item]
+	if !ok || len(positions) == 0 {
+		return fmt.Errorf("%f is not in heap", item)
+	}
+	// remove the last position from the hast table.
+	lastPosition := positions[len(positions)-1]
+	return h.removeAtPosition(lastPosition)
+}
+
+// Contains returns true if the item is in the heap, else false.
+func (h *MinHeap) Contains(item float64) bool {
+	positions, ok := h.hashTable[item]
+	if !ok || len(positions) == 0 {
+		return false
+	}
+	return true
+}
+
+// Size returns the size of the heap.
+func (h *MinHeap) Size() int {
+	return h.length
+}
---
+++ min-heap_test.go
@@ -0,0 +1,394 @@
+package datastructures
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestHeap_Insert(t *testing.T) {
+	tests := []struct {
+		name              string
+		items             []float64
+		want              []float64
+		expectedHashTable map[float64][]int
+	}{
+		{
+			name:  "inserting 3 items",
+			items: []float64{3.2, 4, 2},
+			want:  []float64{2, 4, 3.2},
+			expectedHashTable: map[float64][]int{
+				2:   {0},
+				3.2: {2},
+				4:   {1},
+			},
+		},
+		{
+			name:  "inserting 4 items",
+			items: []float64{3, 4, 2, 1},
+			want:  []float64{1, 2, 3, 4},
+			expectedHashTable: map[float64][]int{
+				1: {0},
+				2: {1},
+				3: {2},
+				4: {3},
+			},
+		},
+		{
+			name:              "inserting no item",
+			expectedHashTable: make(map[float64][]int),
+		},
+		{
+			name:  "inserting 1 item",
+			items: []float64{6},
+			want:  []float64{6},
+			expectedHashTable: map[float64][]int{
+				6: {0},
+			},
+		},
+		{
+			name:  "inserting 10 items",
+			items: []float64{9, 4, 6, 2, 6, 3, 7, 8, 3, 10},
+			want:  []float64{2, 3, 3, 4, 6, 6, 7, 9, 8, 10},
+			expectedHashTable: map[float64][]int{
+				2:  {0},
+				3:  {2, 1},
+				4:  {3},
+				6:  {4, 5},
+				7:  {6},
+				9:  {7},
+				8:  {8},
+				10: {9},
+			},
+		},
+		{
+			name:  "inserting 15 items",
+			items: []float64{9, 4, 6, 2, 6, 3, 7, 8, 3, 10, 5, 11, 1, 8, 100},
+			want:  []float64{1, 3, 2, 4, 5, 3, 7, 9, 8, 10, 6, 11, 6, 8, 100},
+			expectedHashTable: map[float64][]int{
+				1:   {0},
+				2:   {2},
+				3:   {1, 5},
+				4:   {3},
+				5:   {4},
+				6:   {10, 12},
+				7:   {6},
+				8:   {8, 13},
+				9:   {7},
+				10:  {9},
+				11:  {11},
+				100: {14},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := NewMinHeap()
+			for _, item := range tt.items {
+				h.Insert(item)
+			}
+			if h.length != len(tt.want) {
+				t.Errorf("Heap.Insert() = %v, want %v", h.length, len(tt.want))
+			}
+			if !reflect.DeepEqual(h.items, tt.want) {
+				t.Errorf("Heap.Insert() = %v, want %v", h.items, tt.want)
+			}
+			if !reflect.DeepEqual(h.hashTable, tt.expectedHashTable) {
+				t.Errorf("Heap.Insert() hashTable = %v, \n want %v", h.hashTable, tt.expectedHashTable)
+			}
+		})
+	}
+}
+
+func TestHeap_Poll(t *testing.T) {
+	tests := []struct {
+		name              string
+		items             []float64
+		want              []float64
+		pollCount         int
+		expectedHashTable map[float64][]int
+		wantErr           bool
+	}{
+		{
+			name:      "inserting 4 items - polling 2",
+			items:     []float64{3, 5, 2, 6},
+			want:      []float64{6},
+			pollCount: 3,
+			expectedHashTable: map[float64][]int{
+				2: {},
+				3: {},
+				5: {},
+				6: {0},
+			},
+		},
+		{
+			name:              "inserting 1 item - polling 2",
+			items:             []float64{4},
+			pollCount:         2,
+			wantErr:           true,
+			expectedHashTable: map[float64][]int{4: {}},
+		},
+		{
+			name:      "inserting 10 items - polling 9",
+			items:     []float64{9, 4, 6, 2, 6, 3, 7, 8, 3, 10},
+			want:      []float64{10},
+			pollCount: 9,
+			expectedHashTable: map[float64][]int{
+				2:  {},
+				3:  {},
+				4:  {},
+				6:  {},
+				7:  {},
+				9:  {},
+				8:  {},
+				10: {0},
+			},
+		},
+		{
+			name:      "inserting 10 items - polling 5",
+			items:     []float64{9, 4, 6, 2, 6, 3, 7, 8, 3, 10},
+			want:      []float64{6, 8, 7, 10, 9},
+			pollCount: 5,
+			expectedHashTable: map[float64][]int{
+				2:  {},
+				3:  {},
+				4:  {},
+				6:  {0},
+				7:  {2},
+				9:  {4},
+				8:  {1},
+				10: {3},
+			},
+		},
+		{
+			name:      "inserting 15 items - polling 7",
+			items:     []float64{9, 4, 6, 2, 6, 3, 7, 8, 3, 10, 5, 11, 1, 8, 100},
+			want:      []float64{6, 8, 7, 9, 10, 8, 11, 100},
+			pollCount: 7,
+			expectedHashTable: map[float64][]int{
+				1:   {},
+				2:   {},
+				3:   {},
+				4:   {},
+				5:   {},
+				6:   {0},
+				7:   {2},
+				8:   {5, 1},
+				9:   {3},
+				10:  {4},
+				11:  {6},
+				100: {7},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := NewMinHeap()
+			for _, item := range tt.items {
+				h.Insert(item)
+			}
+			for i := 0; i < tt.pollCount; i++ {
+				if err := h.Poll(); err != nil && (err != nil) != tt.wantErr {
+					t.Errorf("Heap.Poll() error = %v, wantErr %v", err, tt.wantErr)
+				}
+			}
+			if h.length != len(tt.want) {
+				t.Errorf("Heap.Poll() = %v, want %v", h.length, len(tt.want))
+			}
+			if !reflect.DeepEqual(h.items, tt.want) {
+				t.Errorf("Heap.Poll() items = %v, \n want %v", h.items, tt.want)
+			}
+			if !reflect.DeepEqual(h.hashTable, tt.expectedHashTable) {
+				t.Errorf("Heap.Poll() hashTable = %v, \n want %v", h.hashTable, tt.expectedHashTable)
+			}
+		})
+	}
+}
+
+func TestHeap_Remove(t *testing.T) {
+	type args struct {
+		item float64
+	}
+	tests := []struct {
+		args              args
+		name              string
+		items             []float64
+		want              []float64
+		expectedHashTable map[float64][]int
+		wantErr           bool
+	}{
+		{
+			name:  "removing first item",
+			items: []float64{20.3, 45.6, 56.2},
+			want:  []float64{45.6, 56.2},
+			args:  args{item: 20.3},
+			expectedHashTable: map[float64][]int{
+				20.3: {},
+				45.6: {0},
+				56.2: {1},
+			},
+		},
+		{
+			name:  "removing invalid item",
+			items: []float64{4, 2, 5, 6, 6, 7},
+			want:  []float64{2, 4, 5, 6, 6, 7},
+			args:  args{item: 3002},
+			expectedHashTable: map[float64][]int{
+				2: {0},
+				4: {1},
+				5: {2},
+				6: {3, 4},
+				7: {5},
+			},
+			wantErr: true,
+		},
+		{
+			name:  "removing a middle item",
+			items: []float64{8, 0.33, 0.44, 0.23, 0.12},
+			want:  []float64{0.12, 0.23, 0.33, 8},
+			args:  args{item: 0.44},
+			expectedHashTable: map[float64][]int{
+				0.44: {},
+				0.12: {0},
+				0.23: {1},
+				8:    {3},
+				0.33: {2},
+			},
+		},
+		{
+			name:  "removing an extreme item",
+			items: []float64{1, 5, 9},
+			want:  []float64{1, 5},
+			args:  args{item: 9},
+			expectedHashTable: map[float64][]int{
+				1: {0},
+				5: {1},
+				9: {},
+			},
+		},
+		{
+			name:              "removing from an empty heap",
+			args:              args{item: 10},
+			wantErr:           true,
+			expectedHashTable: map[float64][]int{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := NewMinHeap()
+			for _, item := range tt.items {
+				h.Insert(item)
+			}
+			if err := h.Remove(tt.args.item); (err != nil) != tt.wantErr {
+				t.Errorf("Heap.Remove() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if h.length != len(tt.want) {
+				t.Errorf("Heap.Remove() = %v, want %v", h.length, len(tt.want))
+			}
+			if !reflect.DeepEqual(h.items, tt.want) {
+				t.Errorf("Heap.Remove() items = %v, \n want %v", h.items, tt.want)
+			}
+			if !reflect.DeepEqual(h.hashTable, tt.expectedHashTable) {
+				t.Errorf("Heap.Remove() hashTable = %v, \n want %v", h.hashTable, tt.expectedHashTable)
+			}
+		})
+	}
+}
+
+func TestHeap_Contains(t *testing.T) {
+	type args struct {
+		item float64
+	}
+	tests := []struct {
+		name  string
+		items []float64
+		args  args
+		want  bool
+	}{
+		{
+			name:  "existing item",
+			items: []float64{40, 10, 22, 55},
+			args:  args{item: 10},
+			want:  true,
+		},
+		{
+			name:  "invalid item",
+			items: []float64{30, 49, 233},
+			args:  args{item: 443},
+		},
+		{
+			name: "invalid item in empty heap",
+			args: args{item: 1344},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := NewMinHeap()
+			for _, item := range tt.items {
+				h.Insert(item)
+			}
+			if got := h.Contains(tt.args.item); got != tt.want {
+				t.Errorf("Heap.Contains() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHeap_Size(t *testing.T) {
+	tests := []struct {
+		name  string
+		items []float64
+		want  int
+	}{
+		{
+			name: "empty heap",
+			want: 0,
+		},
+		{
+			name:  "heap with 6 items",
+			items: []float64{2, 4, 5, 6, 2, 5},
+			want:  6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := NewMinHeap()
+			for _, item := range tt.items {
+				h.Insert(item)
+			}
+			if got := h.Size(); got != tt.want {
+				t.Errorf("Heap.Size() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMinHeap_Insert(t *testing.T) {
+	type fields struct {
+		hashTable map[float64][]int
+		items     []float64
+		length    int
+	}
+	type args struct {
+		item float64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *MinHeap
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := &MinHeap{
+				hashTable: tt.fields.hashTable,
+				items:     tt.fields.items,
+				length:    tt.fields.length,
+			}
+			if got := h.Insert(tt.args.item); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MinHeap.Insert() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
